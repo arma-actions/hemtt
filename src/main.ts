f45464c08057cd46c26f65989df54922a7a2fc8a
BrettMayson <brett@mayson.io>
2023-03-19 15:47:58 -0600

    initial new hemtt action

---
+++ src/main.ts
@@ -0,0 +1,59 @@
+import {Octokit} from '@octokit/rest'
+import * as core from '@actions/core'
+import * as fs from 'fs'
+import * as http from 'http'
+import extract from 'extract-zip'
+
+const isWin = process.platform === 'win32'
+
+const octokit = new Octokit()
+
+const tag: string = core.getInput('version', {trimWhitespace: true})
+
+async function run(): Promise<void> {
+  const release =
+    tag === 'latest'
+      ? await octokit.rest.repos.getLatestRelease({
+          owner: 'BrettMayson',
+          repo: 'HEMTT'
+        })
+      : await octokit.rest.repos.getReleaseByTag({
+          owner: 'BrettMayson',
+          repo: 'HEMTT',
+          tag
+        })
+  const assets = release.data.assets
+  const asset = assets.find(a => {
+    return a.name === (isWin ? 'windows-x64.zip' : 'linux-64.zip')
+  })
+  if (asset === undefined) {
+    core.setFailed(
+      `Could not find ${
+        isWin ? 'windows-x64.zip' : 'linux-x64.zip'
+      } in release ${tag}`
+    )
+    return
+  }
+  core.info(`Downloading ${asset.name} from ${asset.browser_download_url}`)
+  // download the zip
+
+  const file = fs.createWriteStream('hemtt.zip')
+  http.get(asset.browser_download_url, response => {
+    response.pipe(file)
+    file.on('finish', () => {
+      file.close()
+      core.info('Download Completed')
+    })
+  })
+
+  // extract the zip
+
+  await extract('hemtt.zip', {dir: 'hemtt'})
+
+  // add to path
+
+  core.addPath('hemtt')
+  core.info('HEMTT added to path')
+}
+
+run()

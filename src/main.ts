e42508f50d3479398b11c1894b275ee6ab99430b
BrettMayson <brett@mayson.io>
2023-03-19 16:10:06 -0600

    use library

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,27 @@
-import {Octokit} from '@octokit/rest'
 import * as core from '@actions/core'
-import * as fs from 'fs'
-import * as https from 'https'
-import extract from 'extract-zip'
+import {downloadRelease} from '@terascope/fetch-github-release'
 
 const isWin = process.platform === 'win32'
-
-const octokit = new Octokit()
 
 const tag: string = core.getInput('version', {trimWhitespace: true})
 
 async function run(): Promise<void> {
-  const release =
-    tag === 'latest'
-      ? await octokit.rest.repos.getLatestRelease({
-          owner: 'BrettMayson',
-          repo: 'HEMTT'
-        })
-      : await octokit.rest.repos.getReleaseByTag({
-          owner: 'BrettMayson',
-          repo: 'HEMTT',
-          tag
-        })
-  core.info(`HEMTT Version: ${release.data.tag_name}`)
-  core.debug(`Release: ${JSON.stringify(release)}`)
-  const assets = release.data.assets
-  const asset = assets.find(a => {
-    core.debug(`Asset: ${JSON.stringify(a)}`)
-    return a.name === (isWin ? 'windows-x64.zip' : 'linux-x64.zip')
-  })
-  if (asset === undefined) {
-    core.setFailed(
-      `Could not find ${
-        isWin ? 'windows-x64.zip' : 'linux-x64.zip'
-      } in release ${tag}`
-    )
-    return
-  }
-  core.info(`Downloading ${asset.name} from ${asset.browser_download_url}`)
-  // download the zip
-
-  const file = fs.createWriteStream('hemtt.zip')
-  https.get(asset.browser_download_url, response => {
-    response.pipe(file)
-    file.on('finish', async () => {
-      file.close()
-      core.info('Download Completed')
-
-      // extract the zip
-      await extract('hemtt.zip', {dir: `${process.cwd()}/hemtt/`})
-
-      // add to path
-
-      core.addPath('hemtt')
-      core.info('HEMTT added to path')
-    })
-  })
+  await downloadRelease(
+    'BrettMayson',
+    'HEMTT',
+    'hemtt',
+    release => {
+      return release.tag_name === tag
+    },
+    asset => {
+      return isWin
+        ? asset.name === 'windows-x64.zip'
+        : asset.name === 'linux-x64.zip'
+    },
+    false,
+    false
+  )
+  core.addPath('hemtt')
 }
 
 run()
